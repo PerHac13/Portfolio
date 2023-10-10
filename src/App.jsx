3f1871e60959021ac42fa99d7901bb6898ddfd5b
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-10-10 08:28:39 +0530

    Test: Basic run

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.scss";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello SHAIKH ABDULLAH!</h1>
+    </>
+  );
 }
 
 export default App;
