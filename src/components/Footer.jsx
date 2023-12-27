c3d13428b7c950c87d3d7086109e8e99e1804eeb
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-12-27 22:06:46 +0530

    Complete: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,17 @@
 function Footer() {
   return (
-    <div>
-      <h1 className="text-white">Footer</h1>
+    <div className="bottom-0 mt-auto flex-1">
+      <div className="bg-primary text-center w-full pt-20 pb-20">
+        <div className="mx-auto w-[72%] h-[1px] bg-[rgba(112,112,112,.3)]"></div>
+        <div className="">
+          <div className="">
+            <div className="pt-5">
+              Designed and developed with ❤ by Shaikh Abdullah<strong>.</strong>{" "}
+              © 2023 All rights reserved
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
