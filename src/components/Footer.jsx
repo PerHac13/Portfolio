7e2e3d294194b2aadb78a0bb6fcbf413f7704de2
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-12-28 20:34:52 +0530

    Fix: Bottom Padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 function Footer() {
   return (
     <div className="bottom-0 mt-auto flex-1">
-      <div className="bg-primary text-center w-full pt-20 pb-20">
-        <div className="mx-auto w-[72%] h-[1px] bg-[rgba(112,112,112,.3)]"></div>
+      <div className="bg-primary/[130%] text-center w-full pt-20 pb-10">
+        <div className="mx-auto w-[72%] h-[1px] bg-[rgba(112,112,112,.3)] "></div>
         <div className="">
           <div className="">
-            <div className="pt-5">
+            <div className="pt-5 text-[#333333]">
               Designed and developed with ❤ by Shaikh Abdullah<strong>.</strong>{" "}
               © 2023 All rights reserved
             </div>
