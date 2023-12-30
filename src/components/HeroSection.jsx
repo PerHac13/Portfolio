cf81ba10b1c0f92e54bc7166f08dd552246ca10c
Shaikh_Abdullah <abdullah13112004@gmail.com>
2023-12-30 10:38:49 +0530

    Fix: Bug

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,8 +1,18 @@
+import MButton from "./Button";
+
 function HeroSection() {
   return (
     <div className="m-0">
       <div className="text-white">Hola , I am SHAIKH ABDULLAH</div>
       <div className=""> Image</div>
+      <MButton
+        text="Reach Out To Me"
+        bgColor="primary"
+        hoverBgColor="primary"
+        textColor="black"
+        hoverTextColor="black"
+        font="bold"
+      />
     </div>
   );
 }
